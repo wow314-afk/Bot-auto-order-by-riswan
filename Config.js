6c82530a8f4eb7a062aad637c95bc4a1b535c014
wow314-afk <gapentingherlina@gmail.com>
2026-07-28 07:38:17 +0800

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  token: 'YOUR_BOT_TOKEN_HERE',
+  ownerId: 'YOUR_TELEGRAM_ID_HERE',
+  ownerUsername: '@StxrlyYanzz',
+  
+  menuImage: 'https://ziperr-img.lovable.app/i/z6kpgbpl.jpg',
+  
+  qris: {
+    imageUrl: 'https://ziperr-img.lovable.app/i/5ehde5bq.jpg',
+    name: 'ZYRIX PLP PROJECT'
+  },
+  
+  payment: {
+    dana: {
+      number: '085726164070',
+      name: 'RATNA WHAYU'
+    }
+  },
+  
+  prices: {
+    member: 3000,
+    ress: 7000,
+    owner: 9000,
+    base: 10000
+  },
+  
+  base: {},
+  
+  channel: '@zyrixplp',
+  channelLink: 'https://t.me/zyrixplp',
+  orderChannel: '@zyrixplp_order',
+  groupLink: 'https://t.me/zyrixplp_group',
+  
+  admins: ['YOUR_TELEGRAM_ID_HERE'],
+  dbFile: './users.json',
+  orderFile: './orders.json',
+  maintenance: false
+};
